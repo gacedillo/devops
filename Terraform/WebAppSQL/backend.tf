3264040961ff78b9f6b3ad2f2a7369e3c172e6e8
Anthony Cedillo <acedillo@sparkfish.com>
2021-12-23 16:38:51 -0600

    specify cloud only

--- Terraform/WebAppSQL/backend.tf
+++ Terraform/WebAppSQL/backend.tf
@@ -1,11 +1,11 @@
-terraform {
-  backend "azurerm" {
-    # resource_group_name  = "tf_state"
-    # storage_account_name = "tfstate019"
-    # container_name       = "tfstate"
-    # key                  = "terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     # resource_group_name  = "tf_state"
+#     # storage_account_name = "tfstate019"
+#     # container_name       = "tfstate"
+#     # key                  = "terraform.tfstate"
+#   }
+# }
 
 terraform {
   cloud {
